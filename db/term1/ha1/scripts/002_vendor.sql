e0538b5fc75ea755a59f96b05dde6795da3d47f7
fadyat <fadyat@icloud.com>
2024-11-09 18:05:27 +0300

    feat: synced

--- db/term1/ha1/scripts/002_vendor.sql
+++ db/term1/ha1/scripts/002_vendor.sql
@@ -5,52 +5,13 @@
 --  * сколько раз его водитель встречал Новый год в поездке
 --  * максимальную стоимость поездки в среднем на пассажира
 
-with
-avg_ride_duration as (
-    select vendor_id,
-        avg(extract(epoch from taxi_rides.tpep_dropoff_datetime - taxi_rides.tpep_pickup_datetime)) as avg_trip_duration
-    from taxi_rides
-    group by vendor_id
-),
-ride_cost_variance as (
-    select vendor_id,
-        variance(total_amount) as trip_cost_variance
-    from taxi_rides
-    group by vendor_id
-),
-rides_during_work_hours as (
-    select vendor_id,
-        count(*) as trips_during_work_hours
-    from taxi_rides
-    where extract(hour from tpep_pickup_datetime) between 6 and 17
-    group by vendor_id
-),
-rides_on_new_year as (
-    select vendor_id,
-        count(*) as trips_on_new_year
-    from taxi_rides
-    where extract(year from tpep_pickup_datetime) <
-        extract(year from tpep_dropoff_datetime)
-    group by vendor_id
-),
-max_ride_cost_per_passenger as (
-    select vendor_id,
-        max(fare_amount / passenger_count) as max_trip_cost_per_passenger
-    from taxi_rides
-    where passenger_count > 0
-    group by vendor_id
-)
 select
     vendor_id,
-    avg_trip_duration,
-    trip_cost_variance,
-    coalesce(trips_during_work_hours, 0) as trips_during_work_hours,
-    coalesce(trips_on_new_year, 0)       as trips_on_new_year,
-    max_trip_cost_per_passenger
-from
-    avg_ride_duration
-    full join ride_cost_variance using (vendor_id)
-    full join rides_during_work_hours using (vendor_id)
-    full join rides_on_new_year using (vendor_id)
-    full join max_ride_cost_per_passenger using (vendor_id)
-
+    avg(trip_distance) as avg_trip_distance,
+    variance(total_amount) as trip_cost_variance,
+    count(*) filter ( where extract(hour from tpep_pickup_datetime) between 6 and 17 ) as trips_during_work_hours,
+    count(*) filter ( where extract(year from tpep_pickup_datetime) < extract(year from tpep_dropoff_datetime) ) as trips_on_new_year,
+    max(fare_amount / passenger_count) filter ( where passenger_count > 0) as max_trip_cost_per_passenger
+from taxi_rides
+group by vendor_id
+order by vendor_id;
--- db/term1/ha1/scripts/004_gcd.sql
+++ db/term1/ha1/scripts/004_gcd.sql
@@ -13,48 +13,24 @@ create table if not exists nums
     b int check (b > 0)
 );
 
-truncate nums;
-
 insert into nums
 select
     random() * 100 + 1,
     random() * 100 + 1
 from generate_series(1, 10);
 
-create or replace function gcd(a int, b int)
-    returns int as
-$$
-    with recursive gcd_recursive(a, b) as (
-        select a, b
-        union all
-        select b, a % b
-        from gcd_recursive
-        where b != 0
-    )
-    select a
-    from gcd_recursive
-    order by b desc
-    limit 1;
-$$
-language sql;
-
-do $$
-begin
-    assert 1 = gcd(1, 1), 'gcd(1, 1) = 1';
-    assert 5 = gcd(25, 5), 'gcd(25, 5) = 5';
-    assert 1 = gcd(11, 13), 'gcd(11, 13) = 1';
-
-    assert (select count(*) from nums where gcd(a, b) != gcd(b, a)) = 0, 'gcd(a, b) != gcd(b, a)';
-end
-$$
-language plpgsql;
-
-with gcds as (
-    select
-        a,
-        b,
-        gcd(a, b) as gcd
+with recursive gcd as (
+    select a as a0, b as b0, a, b
     from nums
+    union all
+    select a0, b0, b, a % b
+    from gcd
+    where b > 0
 )
-select *
-from gcds;
+select
+    a0 as a,
+    b0 as b,
+    a as gcd
+from gcd
+where b = 0;
+
